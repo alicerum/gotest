1a1fc20371a019dda69a563c1c2b48a91d303e47
Alice Rum <wyvie@wyvie.org>
2022-07-11 00:14:45 +0200

    So this is my super awesome commit

--- main.go
+++ main.go
@@ -2,6 +2,37 @@ package main
 
 import "fmt"
 
+type MyInterface interface {
+	method1(val1 int) int
+	method2(val1, val2 int) int
+}
+
+type defaultImplementation struct {
+	Addition int
+}
+
+func (d *defaultImplementation) method1(val1 int) int {
+	return val1 + d.Addition
+}
+
+func (d *defaultImplementation) method2(val1, val2 int) int {
+	return val1 + val2 + d.Addition
+}
+
+// Now this is fun
+type VeryMuchMyStruct struct {
+	MyInterface
+}
+
+func (m *VeryMuchMyStruct) method1(val1 int) int {
+	return val1 + 1
+}
+
 func main() {
-	fmt.Println("vim-go")
+	var mi MyInterface = &VeryMuchMyStruct{
+		MyInterface: &defaultImplementation{Addition: 3},
+	}
+
+	fmt.Printf("method1 (should add 1 because overrided, so result is 6): %d\n", mi.method1(5))
+	fmt.Printf("method2 (taken from default implementation, so result is 6):%d\n", mi.method2(1, 2))
 }
